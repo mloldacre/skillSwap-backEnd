169ea7da5d4d7668ab0470c60b410bb28aecf2a9
Michael Oldacre <mloldacre@live.com>
2020-09-26 17:36:25 -0400

    Fixed DB connection

--- src/config.js
+++ src/config.js
@@ -1,8 +1,7 @@
 module.exports = {
   PORT: process.env.PORT || 8000,
   NODE_ENV: process.env.NODE_ENV || 'development',
-  DATABASE_URL: process.env.DATABASE_URL || 'postgresql://lifescribe@localhost/lifescribe',
-  TEST_DATABASE_URL: process.env.TEST_DATABASE_URL || 'postgresql://lifescribe@localhost/lifescribe-test',
+  DATABASE_URL: process.env.DATABASE_URL || 'postgresql://brtr@localhost/skillswap',
   API_TOKEN: process.env.API_TOKEN,
   JWT_EXPIRY: process.env.JWT_EXPIRY || '120m'
 };
